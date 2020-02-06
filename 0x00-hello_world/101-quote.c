aafc73b1a3e23d7e3ee19d0ca94202eb0ad4ce8f
Cholado <sntgmtnz@gmail.com>
2020-02-06 14:14:37 +0000

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - echo line to stdout
- * Description: uses echo to output fixed message
+ * main - write line to stdout
+ * Description: uses write to output fixed message
  * Return: 1
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
