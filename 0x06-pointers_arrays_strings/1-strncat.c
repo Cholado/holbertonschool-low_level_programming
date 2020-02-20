4cedb5b36d1cbfb645be37322b97f9521419f362
Cholado <sntgmtnz@gmail.com>
2020-02-20 15:59:09 +0000

    Write a function that concatenates two strings. use at most n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
-/*
- * _strncat - appends the src (n bytes) string to the dest string.
- * @
- * Description: concatenates two strings. if exist overwrite null
- * Return: dest output string
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ *
+ * Return: address of dest
  */
 
 char *_strncat(char *dest, char *src, int n)
