7a509229fc6bc66cf942fdf4a51fef138041d6ed
Cholado <sntgmtnz@gmail.com>
2020-02-22 23:30:35 +0000

    Write a function that adds two numbers.

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -11,34 +11,58 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, temp, remainder, one, two;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	i = j = 0;
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	while (size_r != 0 && i != 0 && j != 0)
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
-		if (n1[0] == '-')
-			one = (n1[i] - '0');
-		else
-			one = (n1[i] - '0') * -1;
-		if (n2[0] == '-')
-			two = (n1[i] - '0');
-		else
-			two = (n1[i] - '0') * -1;
-
-		temp = one + two + remainder;
-		if (temp > 9)
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
 		{
-			remainder = temp / 10;
-			temp %= 10;
+			carry = 1;
+			sum -= 10;
 		}
-		r[size_r--] = temp;
-		printf("%d\n", r[size_r + 1]);
-		i--;
-		j--;
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
 	}
-	return (r[0]);
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
+	return (r);
 }
