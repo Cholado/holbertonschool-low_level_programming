b0828f9034e360932098b5b2cb21100731b5bb59
Cholado <sntgmtnz@gmail.com>
2020-03-19 21:00:00 +0000

    Write a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: ptr to the start of the list
+ * Description: free position and let list return to head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *p, *next;
+
+	p = head;
+	while (current != NULL)
+	{
+		next = p->next;
+		free(p->str);
+		free(p);
+		p = next;
+	}
+
+}
