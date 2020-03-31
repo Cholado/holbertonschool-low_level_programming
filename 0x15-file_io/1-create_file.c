d02ad82030ef44dee03c523a725b0641d8799be2
Cholado <sntgmtnz@gmail.com>
2020-03-31 21:51:57 +0000

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t i;
+	ssize_t i, len;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
 	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-	else
-	{
-		write(fd, text_content, i);
-	}
+		text_content = "";
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	len  = write(fd, text_content, i);
 	close(fd);
+	if (i == -1 || i != len)
+		return (-1);
+
 	return (1);
 }
