54f50b693208e0efe297dfe5c962e638312a0bb2
Cholado <sntgmtnz@gmail.com>
2020-05-03 23:26:55 +0000

    betty trail space

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,7 @@
  * add_dnodeint - add a new node at the beginning of the linked list
  * @head: double pointer to the beginning of the linked list
  * @n: value to add to the new node
- * Description: 
+ * Description: add node before head and become head
  * Return: pointer to the new node, or NULL on failure
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
