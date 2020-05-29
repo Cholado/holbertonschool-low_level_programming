40d8fa8ebf053403cdd4539a62beb863a14bef74
Cholado <sntgmtnz@gmail.com>
2020-05-29 23:29:33 +0000

    Write a function that deletes a hash table.

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ * Description: >>> del ht
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp, *prev;
+	unsigned long int size_ht, i;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+
+	for (i = 0, size_ht = ht->size; i < size_ht; i++)
+	{
+		tmp = ht->array[i];
+		while (temp)
+		{
+			prev = temp;
+			temp = temp->next;
+			free(prev->key);
+			free(prev->value);
+			free(prev);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
