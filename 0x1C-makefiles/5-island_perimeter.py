3acaeae1d8f078d3cd38974c773a2dd915f528a9
Cholado <sntgmtnz@gmail.com>
2020-07-03 19:25:44 -0500

    Mandatory tasks, wrong dir name

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate perimeter of the island.
+    Grid is a list of equal sized lists.
+    """
+    # Measure grid (map) matrix
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+    shores = 0
+
+    # Checks for a valid grid
+    if height == 0:
+        return 0
+
+    if width == 0:
+        return 0
+
+    # Checks for land (1's in the grid)
+    for x in range(height):
+        for y in range(width):
+            if (grid[x][y]):
+                # uses cartography compass to check the grid island's  shores
+                # North
+                if (x > 0 and grid[x - 1][y]):
+                    shores += 1
+                # West
+                if (y > 0 and grid[x][y - 1]):
+                    shores += 1
+                # South
+                if (x < height-1 and grid[x + 1][y]):
+                    shores += 1
+                # East
+                if (y < width-1 and grid[x][y + 1]):
+                    shores += 1
+            # 4 compass directions
+            perimeter += (4 - shores)
+    return perimeter
